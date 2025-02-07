0b6a8ed681ceba04fd99c09a32257af06043b6b8
davidemarcoli <davide@marcoli.ch>
2025-02-07 13:21:25 +0100

    fix(api): update TMDB language handling to use a default value

--- src/app/api/config/tmdb.ts
+++ src/app/api/config/tmdb.ts
@@ -3,7 +3,8 @@ import { env } from 'process'
 const TMDB_READ_ACCESS_TOKEN =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTkxMmVmOWFhM2IxNzg2Zjk3ZTE1NWY1YmQ3ZjY1MSIsInN1YiI6IjY1M2NjNWUyZTg5NGE2MDBmZjE2N2FmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xrIXsMFJpI1o1j5g2QpQcFP1X3AfRjFA5FlBFO5Naw8'
 
-const TMDB_LANGUAGE = env.TMDB_LANGUAGE || 'en-US'
+const DEFAULT_TMDB_LANGUAGE = 'en-US'
+const TMDB_LANGUAGE = env.TMDB_LANGUAGE || DEFAULT_TMDB_LANGUAGE
 
 export const TMDB_CONFIG = {
     baseUrl: 'https://api.themoviedb.org/3',
@@ -11,5 +12,5 @@ export const TMDB_CONFIG = {
         Authorization: `Bearer ${TMDB_READ_ACCESS_TOKEN}`,
         'Content-Type': 'application/json;charset=utf-8'
     },
-    defaultLanguage: 'en-US'
+    defaultLanguage: TMDB_LANGUAGE
 }
