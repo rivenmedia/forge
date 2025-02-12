a4f2ea9aeafafb2d61bf64918c1886939a43f693
Jamie Isaksen <jamie@jamie.no>
2025-02-12 15:39:19 +0100

    Replace drizzle migrations with direct push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'drizzle-kit'
 
 export default {
     schema: './src/lib/db/schema.ts',
-    out: './src/lib/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.POSTGRES_URL!
